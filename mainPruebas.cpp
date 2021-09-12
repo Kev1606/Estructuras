0e18c99058afa38799e19839d101ac51fbba8ba3
Kevin <kevincruz1606@estudiantec.cr>
2021-09-12 14:53:52 -0600

    SelectionSort closed

--- mainPruebas.cpp
+++ mainPruebas.cpp
@@ -4,3 +4,15 @@ arreglo de varios elementos y que se demuestre que efectivamente lo ordena, que 
 funcionan bien las funciones de la lista y lo mismo que dicha lista quede efectivamente bien 
 ordenada
 */
+#include <iostream>
+#include <stdio.h>
+//#include "player.h"
+//#include "playersList.h"
+#include "selectionSort.h"
+//#include "insertionSort.h"
+
+int main() {
+    int arraySize = 5;
+    float array[arraySize] = {3,8,5,9,1};
+    selectionSort(arraySize,array);
+}
--- selectionSort.h
+++ selectionSort.h
@@ -3,3 +3,28 @@ El cual debe recibir un puntero a un arreglo de
     números enteros y proceder a ordenar los números 
     usando el algoritmos de selectionSort. 
 */
+#include <iostream>
+
+using namespace std;
+  
+// Function to sort the numbers using pointers
+void selectionSort(int size, float* array)
+{
+    int temp;
+
+    // Sort the numbers using pointers
+    for (int start = 0; start < size; start++) {
+        for (int process = start + 1; process < size; process++) {
+  
+            if (*(array + process) < *(array + start)) {
+                temp = *(array + start);
+                *(array + start) = *(array + process);
+                *(array + process) = temp;
+            }
+        }
+    }
+  
+    // print the numbers
+    for (int i = 0; i < size; i++)
+        cout << *(array + i) << "  ";
+}
